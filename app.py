c017f07f66ab243a92e7dc99c707f46a65f2b13e
Monacrylic <siddkot@gmail.com>
2019-09-24 21:07:43 +0530

    added inspire page

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/inspire')
+def inspire():
+    return render_template('inspire.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
