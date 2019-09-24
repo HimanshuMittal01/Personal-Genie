5edf46ec908f06cc6292cc2d276b533792d5062b
Monacrylic <siddkot@gmail.com>
2019-09-25 01:55:44 +0530

    Added inspire me-question1 screen

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
+import csv
+
 
 app = Flask(__name__)
+answers =[]
+questions=[]
+
+@app.route('/tvsearch')
+def tvsearch():
+    f = open("question_sets/tvset.txt")
+    questions=f.readlines()
+    f.close()
+    return render_template('tvsearch.html', question=questions[0])
 
 @app.route('/')
 def index():
@@ -8,7 +19,7 @@ def index():
 
 @app.route('/inspire')
 def inspire():
-    return render_template('inspire.html')
+    return render_template('inspireme.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
