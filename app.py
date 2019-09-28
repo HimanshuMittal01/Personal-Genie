fedbf88566fa57d76ebf48cdc09bb72b2b7e5854
Monacrylic <siddkot@gmail.com>
2019-09-28 15:35:30 +0530

    Question- Answer navigation added successfully

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from flask_wtf import FlaskForm
+
+from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
+from wtforms import validators, ValidationError
 import csv
 
+#answers can be accessed from the answers[] array
+#Questions need to be written in the questions.txt file
+#corresponding options need to be filled in the tvops.csv file
 
 app = Flask(__name__)
 answers =[]
 questions=[]
+app.secret_key = 'development key'
 
-@app.route('/tvsearch')
+qno=0
+all_options=[]
+
+with open('question_sets/tvops.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    
+    for row in csv_reader:
+        all_options.append(row)
+    
+
+class QuestionForm(FlaskForm):
+   
+   Options = RadioField('Options', choices = [('1',all_options[qno][0]),('2',all_options[qno][1]),('3',all_options[qno][2])] )
+
+   submit = SubmitField("Send")
+
+
+@app.route('/tvsearch', methods=['GET','POST'])
 def tvsearch():
     f = open("question_sets/tvset.txt")
     questions=f.readlines()
     f.close()
-    return render_template('tvsearch.html', question=questions[0])
+    form = QuestionForm()
+    global qno
+    if request.method == 'POST':
+        #Render the same screen when no radio button is selected
+        if form.validate() == False:
+            return render_template('tvsearch.html', form = form, question=questions[qno])
+        else:
+            #re-initializing the form everytime to update options
+            qno=qno+1
+            answers.append(form.Options.data)
+            print(answers)
+            form.Options.choices=[('1',all_options[qno][0]),('2',all_options[qno][1]),('3',all_options[qno][2])] 
+            return render_template('tvsearch.html', form=form, question=questions[qno])
+
+    elif request.method == 'GET':
+        # this part runs for the first question
+        return render_template('tvsearch.html', form = form, question=questions[qno] )
+
+
+    
 
 @app.route('/')
 def index():
@@ -23,4 +67,3 @@ def inspire():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
